c41f33f5f4ca1cfd7853897dee5a3c0c043336e0
circle514 <josephchristianmutia@gmail.com>
2026-05-03 18:17:56 +0800

    Revert "I am the Green Ball of Doom"
    This reverts commit 536618af3bdf90b1b2fb8452372e39fa283bf2d8.

--- Game.h
+++ Game.h
@@ -4,28 +4,19 @@
 #include <SFML/Window.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Network.hpp>
+#include <iostream>
 
 /* Game Logic */
-using namespace sf;
 
 class Game
 {
 private:
-	//Variables
-	//Window
-	RenderWindow* window;
-	CircleShape player;
-
 	void initVariables();
 	void initWindow();
-	void initPlayer();
 
 public: 
 	Game();
 	virtual ~Game();
-
-	//Accessors
-	const bool running();
 
 	//Functions
 	void update();
--- main.cpp
+++ main.cpp
@@ -1,13 +1,21 @@
-#include <iostream>
 #include "Game.h"
 
+using namespace sf;
 
 int main() {
 
-	Game game;
+	RenderWindow window(VideoMode({ 1920, 1080 }), "Elementalist", Style::Default);
+	window.setFramerateLimit(60);
+	View gameView(FloatRect({ 0, 0 }, { 320, 180 }));
+	window.setView(gameView);
 
-	while (game.running()) {
-		game.update();	
-		game.render();
+	while (window.isOpen()) {
+		while (const std::optional event = window.pollEvent()) {
+			if (event->is<Event::Closed>()) {
+				window.close();
+			}
+		}
+		window.clear(Color::Blue);
+		window.display();
 	}
 }
